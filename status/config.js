165cb8aabab7c3353fc7e3cf7a7f9465876000e0
NiuFuyu <me@off.cx>
2022-11-06 14:50:22 +0800

    添加新功能~
    Co-Authored-By: NiuFuyu <me@off.cx>

---
+++ status/config.js
@@ -0,0 +1,14 @@
+window.Config = {
+    SiteName: "Mr. Niu's Status",
+    SiteUrl: 'https://Niufuyu855.github.io/status/',
+    ApiDomain: 'api.uptimerobot.com',
+    ApiKeys: [
+        'ur1653018-89f2782006beb859e2e0ccf1', // Mr. Niu小站&Mr. Niu导航
+    ],
+    ShowLink: false,
+    CountDays: 45,
+    Navi: [{
+        text: '回到主页',
+        url: 'https://Niufuyu855.github.io/'
+    }]
+};
