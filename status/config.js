3cbe6603bfd0e88be38a3365de8d3b3ab01f51ef
NiuFuyu <me@off.cx>
2022-11-06 15:47:49 +0800

    修复
    Co-Authored-By: NiuFuyu <me@off.cx>

--- status/config.js
+++ status/config.js
@@ -1,14 +1,14 @@
 window.Config = {
-    SiteName: "Mr. Niu's Status",
-    SiteUrl: 'https://Niufuyu855.github.io/status/',
+    SiteName: "Niute's Status",
+    SiteUrl: 'https://7d8.cn/status/',
     ApiDomain: 'api.uptimerobot.com',
     ApiKeys: [
-        'ur1653018-89f2782006beb859e2e0ccf1', // Mr. Niu小站&Mr. Niu导航
+        'ur1653018-89f2782006beb859e2e0ccf1', // Niute Fony
     ],
     ShowLink: false,
     CountDays: 45,
     Navi: [{
         text: '回到主页',
-        url: 'https://Niufuyu855.github.io/'
+        url: 'https://7d8.cn'
     }]
 };
